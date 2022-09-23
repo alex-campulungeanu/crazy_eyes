02c857d0feab0db53a41e072d23d7c45d3bd9652
Alex Campulungeanu <allecs1987@yahoo.com>
2022-09-23 12:21:43 +0300

    initial

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const anchor = document.getElementById('anchor')
+const rekt = anchor.getBoundingClientRect()
+const anchorX = rekt.left + rekt.width / 2
+const anchorY = rekt.top + rekt.height / 2
+const eyesContainer = document.querySelectorAll('.eye-container')
+
+document.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX
+    const mouseY = e.clientY
+
+    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY)
+
+    eyesContainer.forEach(eye => {
+        eye.style.transform = `rotate(${90 + angleDeg}deg)`
+        anchor.style.filter = `hue-rotate(${angleDeg}deg)`
+    })
+})
+
+function angle(cx, cy, ex, ey) {
+    const dy = ey - cy
+    const dx = ex - cx
+    const rad = Math.atan2(dy, dx)
+    const deg = rad * 180 / Math.PI
+    return deg
+}
